9072603ee587f3f6ea36a3a51aa40355e56bd435
Tara Sherman <taradelin@gmail.com>
2019-10-20 17:01:08 -0400

    updated App to show TodoList

--- reducer-todo/src/App.js
+++ reducer-todo/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Todo from './components/Todo';
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Todo />
+      <TodoList />
     </div>
   );
 }
