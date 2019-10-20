d7fab1aee597ef4ee8cd41e7bb5f12eea093c487
Tara Sherman <taradelin@gmail.com>
2019-10-20 19:36:40 -0400

    Initial state, reducer with cases for adding, completed, and clearing completed

--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -1,40 +1,44 @@
-/* STEP 1 - Build a simple reducer and initial state
-In a folder called reducers add a reducer file and build out a simple reducer with just a default return for now
-In the same file, build your initial state object that has a list of todos. Export both the reducer and the initial state object */
-
-export const initialState = [
+/* Set up initial state as an array of tasks in a variable named todo. Each item in the array has a todo, an id, and a boolean for completed (set to false) */
+export const todo = [
   {
-    task: 'Explore strange new worlds',
+    todo: 'Explore strange new worlds',
     id: 1800,
     completed: false
 },
 {
-    task: 'Seek out new life and new civilizations',
+    todo: 'Seek out new life and new civilizations',
     id: 1830,
     completed: false
 },
 {
-    task: 'Boldly go where no man has gone before',
+    todo: 'Boldly go where no man has gone before',
     id: 1900,
     completed: false
 }
 ];
 
-/* Write the case in your reducer for adding a todo (You can create a unique id with new Date()) 
-Write the `case` in your reducer for toggling the completed property
-Write the `case` in your reducer for filtering completed todos*/
+/* (Reminder: The reducer is a pure function that takes 2 arguments (previous state and an action), and returns the next state. (previousState, action) => newState.)
+
+This reducer (stored in a variable called reducer) takes in 2 objects: arguments called state and action (payload of information). The type of the action is a switch statement (
+  1. evaluated once
+  2. value compared with the values of each case
+  3. if there is a match, the associated block of code is executed
+  4. If there is no match, the todo is returned.
+
+The cases here are to add (return payload, i.e. list of todos with new todo appended), toggle completed (map over the array in state, if the id of a todo matches the payload, then return the list of todos with that specific todo marked as completed), or clear completed (filter out any todo marked as completed and return state, i.e. the list of todos remaining after filtering).
+*/
 export const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TODO":
       return [
         ...state,
         {
-          item: action.payload,
+          todo: action.payload,
           id: Date.now(),
           completed: false
         }
       ]
-    case "TOGGLE_COMPLETE":
+    case "TOGGLE_COMPLETED":
       return state.map(todo => {
         if (todo.id === action.payload) {
           return {
@@ -48,5 +52,15 @@ export const reducer = (state, action) => {
 
     case "CLEAR_COMPLETED":
       return state.filter(todo => !todo.completed);
+      default:
+        return state;
   };
 };
+
+/* STEP 1 - Build a simple reducer and initial state
+In a folder called reducers add a reducer file and build out a simple reducer with just a default return for now
+In the same file, build your initial state object that has a list of todos. Export both the reducer and the initial state object */
+
+/* Write the case in your reducer for adding a todo (You can create a unique id with new Date()) 
+Write the `case` in your reducer for toggling the completed property
+Write the `case` in your reducer for filtering completed todos*/
